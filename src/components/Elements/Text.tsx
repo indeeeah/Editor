042974c26b02daae7692fb55d4f6066da729848a
indeeeah <indeeeah@gmail.com>
2024-10-02 15:57:49 +0900

    텍스트 선택 작업 중

--- src/components/Elements/Text.tsx
+++ src/components/Elements/Text.tsx
@@ -1,12 +1,22 @@
 import { forwardRef } from 'react';
 
 import { convertAlign, convertStyle } from '@/libs/style';
+import { useElements } from '@/providers/ElementProvider';
 import { ElementProps } from '@/types/element';
 
 const Text = forwardRef<HTMLDivElement, ElementProps>(({ props }, ref) => {
+  const { elements, setSelectedElement } = useElements();
+
   return (
     <div className="w-full" style={convertAlign(props.style)}>
-      <div ref={ref} onClick={props.onClick}>
+      <div
+        ref={ref}
+        onClick={() => {
+          const element = elements.find(e => e.id === props.id);
+          if (!element) return;
+          setSelectedElement(element);
+        }}
+      >
         {props.value!.split('\n').map((line: string, index: number) => (
           <p key={index} style={{ ...convertStyle(props.style) }}>
             {line}
